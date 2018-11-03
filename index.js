767ca7f03847118844056150b173a64b6f10d0b5
punwai <punwai@yahoo.com>
2018-11-03 15:32:11 +0000

    chat commit

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ var http = require('http').Server(app);
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/client/index.html');
 });
+
+http.listen(80, function(){
+    console.log('listening on *:80');
+  });  
